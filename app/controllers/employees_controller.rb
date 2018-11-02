c52cecd1c2b1707bea44814aced2750040ea7791
Ben Ricker <bgricker@bgricker.com>
2018-11-02 10:38:24 -0400

    seed employee data, render index as json

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,6 +5,7 @@ class EmployeesController < ApplicationController
   # GET /employees.json
   def index
     @employees = Employee.all
+    render json: @employees
   end
 
   # GET /employees/1
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'rocketeers.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+    puts row.to_hash
+end
+
+csv.each do |row|
+  t = Employee.new
+  t.role = row['Focus Area']
+  t.name = row['Name']
+  t.location = row['Location']
+  t.full_time = row['Full-time?']
+  t.save
+end
+
+puts "There are now #{Employee.count} rows in the employee table"
